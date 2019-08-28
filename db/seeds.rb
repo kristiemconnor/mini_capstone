4b5936e78fd3d97f77b47be7babb1c0f130a9923
kristie <kristiemconnor@gmail.com>
2019-08-28 12:59:38 -0400

    adds products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(
+  name: "Starburst", 
+  price: 1,
+  image_url: "https://actualize-onl-2019-07.slack.com/archives/DLKKCSJTW/p1567011178000200",
+  description: "juicy",
+  )
+product.save
+
+product = Product.new(
+  name: "Kit Kats", 
+  price: 2,
+  image_url: "https://www.shelflife.ie/wp-content/uploads/2018/04/KitKat-350x350.jpg",
+  description: "crunchy",
+  )
+product.save
+
+product = Product.new(
+  name: "Mints", 
+  price: 3,
+  image_url: "https://actualize-onl-2019-07.slack.com/archives/DLKKCSJTW/p1567011266000800",
+  description: "fresh",
+  )
+product.save
