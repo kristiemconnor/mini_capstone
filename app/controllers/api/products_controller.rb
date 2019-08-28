f8f8566dbe97935240d52b94202a36c4ab510889
kristie <kristiemconnor@gmail.com>
2019-08-28 13:39:09 -0400

    makes routes for individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,20 @@
 class Api::ProductsController < ApplicationController
+
+  def all_products_action
+    @products = Product.all
+
+    render 'all_products.json.jb'
+
+
+  end
+
+  def product_action
+    @product = Product.first 
+
+    render 'product.json.jb'
+
+
+  end
+
+
 end
