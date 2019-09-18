42a4cf9a6f483b22efacf63a5e5c0d97d41adc59
kristie <kristiemconnor@gmail.com>
2019-09-18 14:17:49 -0400

    updates names for products 4,5. updates descriptions and prices.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,9 +5,38 @@ class ProductsController < ApplicationController
     render 'index.html.erb' 
   end
 
-
   def show
     @product = Product.find(params[:id])
     render 'show.html.erb'
   end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price].to_i,
+      image_url: params[:image_url],
+      description: params[:description],
+      supplier_id: params[:supplier_id]
+      )
+    @product.save
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update 
+    @product = Product.find(params[:id])
+    @product.update(
+      name: params[:name],
+      price: params[:price].to_i,
+      image_url: params[:image_url],
+      description: params[:description],
+      supplier_id: params[:supplier_id]
+      )
+    redirect_to "/products/#{@product.id}"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   get "/products" => "products#index"
   # get "/products/new" => "products#new"
   get "/products/:id" => "products#show"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
+
+  get "/products/:id/edit" => "products#edit"
+  patch "/products/:id" => "products#update"
+
+
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   # namespace :api do
@@ -22,3 +29,4 @@ Rails.application.routes.draw do
     post "/carted_products" => "carted_products#create"
   end
 end
+
