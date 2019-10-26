79e98d430988ee407dbfe3eddf19242f9acfee86
kristie <kristiemconnor@gmail.com>
2019-10-26 17:27:11 -0400

    adds user update action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
-  get "/products" => "products#index"
+  # get "/products" => "products#index"
+  # # get "/products/new" => "products#new"
+  # get "/products/:id" => "products#show"
   # get "/products/new" => "products#new"
-  get "/products/:id" => "products#show"
-  get "/products/new" => "products#new"
-  post "/products" => "products#create"
+  # post "/products" => "products#create"
 
-  get "/products/:id/edit" => "products#edit"
-  patch "/products/:id" => "products#update"
+  # get "/products/:id/edit" => "products#edit"
+  # patch "/products/:id" => "products#update"
 
 
 
---
+++ public/index.js
@@ -0,0 +1,6 @@
+/*global axios*/
+
+axios.get("localhost:3000/api/products").then(function(response) {
+  var products = response.data;
+  console.log(products);
+});
